1688f87402139c0f0f4c88ded40679c8a7602643
achal6076 <Achal6076@gmail.com>
2023-03-06 10:28:36 +0530

    update slider div created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent">
+      <div className="Dashboard"></div>
+
+      <h3> Back</h3>
+      <div className="calculator">
+        {/* <h3>SIP</h3> */}
+        <div className="stepUpCalculaor">
+          <h3> Step calculator section</h3>
+          <div className="rangeSlider">
+            <input type="range" />
+            <svg width="100%" height="24">
+              <line
+                x1="4"
+                y1="0"
+                x2="440"
+                y2="0"
+                stroke="#444"
+                stroke-width="20"
+                stroke-dasharray="1 20"
+              />
+            </svg>
+            <br />
+            <input type="range" />
+            <svg width="100%" height="24">
+              <line
+                x1="4"
+                y1="0"
+                x2="440"
+                y2="0"
+                stroke="#444"
+                stroke-width="20"
+                stroke-dasharray="1 20"
+              />
+            </svg>
+            <br />
+            <input type="range" />
+            <svg width="100%" height="24">
+              <line
+                x1="4"
+                y1="0"
+                x2="440"
+                y2="0"
+                stroke="#444"
+                stroke-width="20"
+                stroke-dasharray="1 20"
+              />
+            </svg>
+            <br />
+            
+            <input type="range" />
+            <svg width="100%" height="24">
+              <line
+                x1="4"
+                y1="0"
+                x2="440"
+                y2="0"
+                stroke="#444"
+                stroke-width="20"
+                stroke-dasharray="1 20"
+              />
+            </svg>
+          </div>
+        </div>
+
+        <div className="graph">
+          <h4>After 5 year's you will have</h4>
+        </div>
+      </div>
     </div>
   );
 }
