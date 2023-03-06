4091aac92b24c3402236b8b6af1cf6dfc5205787
kritika2707 <kritika.14052@gnindia.dronacharya.info>
2023-03-06 12:52:48 +0530

    Updates: Graph added

--- src/Assets/Graph.js
+++ src/Assets/Graph.js
@@ -1,9 +1,13 @@
 import React from "react";
+import GraphComponent from '../components/graphComponent';
 
 function Graph(){
  return (
     <div className="graph">
           <h4>After 5 year's you will have</h4>
+          <h2>8,56,981</h2>
+          <p>That's 1,24,369 as potential capital gains on your investment of 7,32,612</p>
+          <GraphComponent/>
         </div>
  )   
 }
