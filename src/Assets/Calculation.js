20ef0bfa21a2760e47df7c918e3a627465b3c69a
kritika2707 <kritika.14052@gnindia.dronacharya.info>
2023-03-13 18:26:47 +0530

    local changes on hold

---
+++ src/Assets/Calculation.js
@@ -0,0 +1,19 @@
+// import React from "react";
+
+// function Calculation(){
+//     PeriodInMonth = (InvestmentPeriod)*12;
+//     RateOfReturn = (Rateofreturn)/1200;
+//     for(var i=1; i<=PeriodInMonth; i++)
+//     {        
+//     if(i!==1)
+//     {
+//     if(i%12===1)
+//     {
+//     incrementedAmount=Math.floor(MonthlyInvestmentAmount*(YearlyIncrement/100));
+//     MonthlyInvestmentAmount=MonthlyInvestmentAmount + incrementedAmount;
+//     }
+//                 }
+//     CummulationAmount =MonthlyInvestmentAmount*(Math.pow((1+RateOfReturn),(PeriodInMonth-i+1)));        
+//     TotalSIPWithStepUp += CummulationAmount;             
+//     }
+// }
--- src/Assets/Graph.js
+++ src/Assets/Graph.js
@@ -1,24 +1,30 @@
 import React from "react";
 // import GraphComponent from '../components/graphComponent';
-import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+// import Calculation from "./Calculation";
 
 function Graph(){
   const data = [
     {
       name: '0',
+      value: 0,
     },
     {
       name: '1',
+      value: 95000,
     },
     {
       name: '2',
+      value: 190000,
     },
     {
       name: '3',
+      value: 285000,
     },
     {
       name: '4',
+      value: 380000,
     },
     {
       name: '5',
@@ -27,16 +33,14 @@ function Graph(){
   
  return (
   <div className="graph">
-    {/* <GraphComponent> */}
       <div className="textforgraph"><span Style="color:grey;margin-left:10px;margin-top:10px">After <span  Style="color:black;font-weight:bold">5 year's</span> you will have</span>
       <h2 Style="color:#2C74B3;margin-left:-10px;font-weight:bold;margin-top:0"><CurrencyRupeeIcon/>8,56,981</h2>
       <p Style="color:grey;margin-left:12px;margin-top:-19px">That's<span Style="color:#FF6E31;margin-left:-15px;font-weight:bold"><CurrencyRupeeIcon/>1,24,369</span> as potential capital gains on your investment of<span Style="color:#03C988;margin-left:-15px;font-weight:bold"><CurrencyRupeeIcon/>7,32,612</span></p>
       </div>
-    {/* </GraphComponent> */}
-    <ResponsiveContainer className="graph-div" width="100%" height="60%">
+    <ResponsiveContainer className="graph-div" width="100%" height="65%">
         <LineChart
           width={450}
-          height={300}
+          height={450}
           data={data}
           margin={{
             top: 70,
@@ -47,11 +51,10 @@ function Graph(){
         >
         
           <XAxis dataKey="name" />
-          <YAxis ticks={data}/>
+          <YAxis dataKey="value"/>
           <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#2B3467" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#362FD9" />
+          <Line type="monotone" stroke="#2B3467" />
+          <Line type="monotone" stroke="#362FD9" />
         </LineChart>
       </ResponsiveContainer>
   </div>
